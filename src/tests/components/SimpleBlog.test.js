6fb527ae8f5fc87c20b922c9f45b79f1fd6d4aae
Victor Molero <victor.molero@gmail.com>
2019-12-26 03:59:28 +0100

    add setupTest file

--- src/tests/components/SimpleBlog.test.js
+++ src/tests/components/SimpleBlog.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
+import '../setupTests';
 import { render, fireEvent } from '@testing-library/react';
 import SimpleBlog from '../../components/SimpleBlog';
 
